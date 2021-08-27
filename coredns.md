0f04d0ddef82a0622d548ffc99ca14fdd39e9a65
dlystra <dlystra@redhat.com>
2021-08-27 07:44:05 -0500

    added coredns instructions and script

---
+++ coredns.md
@@ -0,0 +1,98 @@
+#  CodeSparta CoreDNS Install
+
+## Devkit VPC
+1. Clone Sparta Devkit VPC repo
+```
+git clone --branch devkit-coredns https://github.com/CodeSparta/devkit-vpc.git && cd devkit-vpc
+```
+2. Configure variables
+```
+vi variables.tf
+```
+3. Exec into Konductor IaC Runtime
+```
+./devkit-build-vpc.sh -vv -e aws_cloud_region=us-gov-west-1 \
+ -e aws_access_key=xxxxxxxxxxxxx -e aws_secret_key=XXXXXXXXXXXXXXXXX
+ ```
+## Low Side Bundle
+1. Push AWS SSH keys to “low side” RHEL Bastion
+```
+scp -i ~/.ssh/${keyname} ~/.ssh/${keyname}* ec2-user@${rhel_bastion_public_ip}:~/.ssh/
+```
+2. SSH to the RHEL bastion
+```
+ssh -i ~/.ssh/${keyname} ec2-user@${rhel_bastion_public_ip}
+```
+3. Ensure Podman is installed
+```
+sudo dnf install -y podman
+```
+4. Create Platform Artifacts Staging Directory
+```
+mkdir -p $(pwd)/bundle
+```
+5. Build Koffer Bundles for CloudCtl, OCP Platform Infra, and Sparta IaC
+```
+podman run -it --rm --pull always \
+--volume ${HOME}/bundle:/root/bundle:z \
+quay.io/cloudctl/koffer:v00.21.0803 bundle \
+--config /root/bundle/sparta.yml \
+-v "latest-4.7"
+```
+6. Paste [Quay.io Image Pull Secret](https://cloud.redhat.com/openshift/install/metal/user-provisioned) when prompted
+
+7. Set Permissions on bundle(s)
+```
+sudo chown -R $USER $(pwd)/bundle
+```
+8. Review your artifacts
+```
+du -sh $(pwd)/bundle/*
+```
+## Airgap Artifact Walk
+1. Push artifact bundles to “high side” RHCOS private registry node
+```
+rsync --progress -avzh bundle -e "ssh -i ~/.ssh/${keyname}" core@${rhcos_private_registry_node_ip}:~
+```
+2. SSH to the “high side” RHCOS CloudCtl private deployment services node
+```
+ssh -i ~/.ssh/${keyname} core@${rhcos_private_registry_node_ip}
+```
+3. Extract bundles
+```
+sudo tar xvf ${HOME}/bundle/koffer-bundle.sparta-*-*.tar -C /root
+```
+## High-Side Deployment
+1. Acquire root
+```
+sudo -i
+```
+2. Change directory to /root/cloudctl and change start_dns var to true
+```
+cd /root/cloudctl; sed -i 's/start_dns: false/start_dns: true/g' vars/run.yml
+```
+3. Run init.sh
+```
+./init.sh
+```
+4. Exec into Konductor
+```
+podman exec -it konductor connect
+```
+4. Assign variables
+```
+vim /root/platform/iac/cluster-vars.yml
+```
+5. Change directory to /root/platform/iac/sparta
+```
+cd /root/platform/iac/sparta
+```
+6. 
+7. Watch Cluster Operators come online (may take 30-60 minutes)
+```
+watch oc get co
+```
+8. Also watch for & add Apps ELB Route53 DNS CNAME `*.apps.cluster.domain.com` wildcard [DNS Entry](https://console.amazonaws-us-gov.com/route53/home?#resource-record-sets)
+```
+ oc get svc -n openshift-ingress | awk '/router-default/{print $4}'
+```
---
+++ coredns.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+cat <<EOF > /root/platform/iac/sparta/optionset.yml
+---
+- name: Set DHCP Option Set
+  hosts: cloudctl
+  vars_files:
+  - '../cluster-vars.yml'
+  - 'vars/global.yml'
+  - 'vars/{{ target_environment }}.yml'
+  vars:
+    listen_address: "{{ lookup('env', 'PUBLISH_ADDRESS') | default(ansible_default_ipv4.address, true) }}"
+  tasks:  
+    - name: set ec2 dns optionset
+      amazon.aws.ec2_vpc_dhcp_option:
+        aws_access_key: "{{ aws_access_key_id }}"
+        aws_secret_key: "{{ aws_secret_access_key }}"
+        domain_name: "{{ cluster_domain }}"
+        region: "{{ aws_region }}"
+        vpc_id: "{{ vpc_id }}"
+        dns_servers:
+          - "{{ listen_address }}"
+        inherit_existing: True
+        delete_old: True
+        tags:
+          Name: "cloudctl-{{ vpc_name }}"
+      delegate_to: konductor
+EOF
+
+cat <<EOF > /root/platform/iac/sparta/shaman.yml
+#!/usr/local/bin/ansible-playbook
+
+- name: ' Konductor UPI '
+  hosts: konductor
+  vars_files:
+  - '../cluster-vars.yml'
+  - 'vars/global.yml'
+  - 'vars/{{ target_environment }}.yml'
+  vars:
+    module: "build"
+    state_provider: "local"
+    tf_module_path: "{{ dir_terraform }}/shaman"
+    vars_module_path: "{{ dir_terraform }}"
+    ansible_name_module: " Konductor | UPI | {{ module }}"
+  tasks:
+    ####### Terraform Apply
+    - name: '{{ ansible_name_module }} | terraform | apply'
+      terraform:
+        project_path: "{{ item }}"
+        variables_file: "{{ vars_module_path }}/global.tfvars"
+        state: present
+      loop:
+        - "{{ tf_module_path }}/elb"
+        - "{{ tf_module_path }}/control-plane"
+      register: tf_output
+EOF
+
+cat <<EOF > /root/platform/iac/sparta/roles/coredns/tasks/aws.yml
+---
+- name: Gather information about all ELBs
+  community.aws.elb_application_lb_info:
+    names: "{{ cluster_name }}-int"
+    region: "{{ aws_region }}"
+    aws_access_key: "{{ aws_access_key_id }}"
+    aws_secret_key: "{{ aws_secret_access_key }}"
+  register: elb_int
+  delegate_to: konductor
+
+- name: 'set fact'
+  set_fact:
+    elb_int_fqdn: "{{ elb_int.load_balancers | map(attribute='dns_name') | first }}"
+  delegate_to: konductor
+EOF
+
+cat <<EOF > /root/platform/iac/sparta/site.yml
+#!/usr/local/bin/ansible-playbook
+- name: 'RedHat | Konductor | site.yml'
+  hosts: koffer
+
+- import_playbook: optionset.yml   # Create DHCP optionset
+- import_playbook: tree.yml        # Create base deployment directory tree 
+- import_playbook: ssl.yml         # Stage deployment secrets
+- import_playbook: secrets.yml     # Stage deployment secrets
+- import_playbook: templates.yml   # Template prerequisite configs & auxillary manifests 
+- import_playbook: registry.yml    # Restart registry with new certificate
+- import_playbook: manifests.yml   # Create & customize OpenShift manifest files
+- import_playbook: ignition.yml    # Build RH CoreOS ignition files from manifests
+- import_playbook: shaman.yml      # Build the ELB and OCP Control plane via Terraform
+- import_playbook: dns.yml         # Configure CoreDNS "BYO DNS" Deploy time NS
+EOF
+
+sed -i 's/{{ elb_name | default(null) }}/{{ elb_name if elb_name is defined }}/g' /root/platform/iac/sparta/templates/coredns/core.db.j2
+
+./site.yml
